2de7795900e00dc721648a25bdf86001c2402729
Jake Carlson <jake@carlsonville.com>
2023-09-10 20:40:43 -0500

    Initial test.

---
+++ src/OcrAutoCorrect/OcrAutoCorrect.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace OcrAutoCorrect;
+
+class OcrAutoCorrect
+{
+
+    public function __construct()
+    {
+
+    }
+
+    public static function hello() {
+        echo "hello\n";
+    }
+
+}
---
+++ src/OcrAutoCorrect/OcrAutoCorrectServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace OcrAutoCorrect;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class PasswordServiceProvider
+ */
+class OcrAutoCorrectServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton('ocrAutoCorrect', OcrAutoCorrect::class);
+    }
+}
